34f3e10df1ffdbce71f910d25e9d3c83fd9e0d19
Tori Acer <vrice360@gmail.com>
2022-03-05 12:01:12 -0600

    set up of html, notes for js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// What does "===" operator mean in Javascript?
+
+// <input class="form-check-input" type="checkbox" value="yes">
+// <label>
+// Will return true 
+// </label> <br>
+
+// <input class="form-check-input" type="checkbox" value="yes">
+// <label>
+// Decremental Operator
+// </label> <br>
+
+// <input class="form-check-input" type="checkbox" value="yes">
+// <label>
+// Equality Coercion
+// </label>
+// </div> */}
+
+// //array of question
+// /data tpe of question
+// // question can be a string
+
+// //array of objects
+
+// set it to a variable
+// questions
+// array of objects 

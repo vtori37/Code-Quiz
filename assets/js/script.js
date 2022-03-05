8036d948b5ac10891160faee0ea5b361a1cbff1d
Tori Acer <vrice360@gmail.com>
2022-03-05 12:26:38 -0600

    develop

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Question what does "===" mean in javascript?
+// incremental operand
+// equality operator
+// equality coercion 
+
+// have variables to questions
+// have an array of objects (which are the answers)

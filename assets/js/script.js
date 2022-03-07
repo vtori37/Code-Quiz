5e97c776ea8f57f655ec5947582e0d4cd645a686
Tori Acer <vrice360@gmail.com>
2022-03-06 20:06:19 -0600

    Attempt to create function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,41 @@
-const questions= [
-  {
+// User Score
+var score = 0;
+// The index of the current question
+var questionNumber = 0;
+//Time left
+var timer = 90;
+//Time deduction for incorrect answers
+var deductions = 10;
+
+//Question Bank
+const questionBank= [
+  { num: 1,
     question: "1. Which of the following is the AND operator?",
-    answersChoices: [
+    answerChoices: [
       "a. ===", 
       "b. ||", 
       "c. &&"],
     answer: 2
   },
-  {
+  { num: 2,
     question: "2. Which of the following contains a string?",
-    answersChoices: [
+    answerChoices: [
       "a. let videogame = 'Please play Breat of the Wild.';", 
       "b. const costs = 2;", 
       "c. var animalTypes = [bear, wolf, elephant, giraffe]"],
-    answer: 09
+    answer: 0
   }, 
-  {
+  { num: 3,
     question: "3. What does JSON stand for?",
-    answersChoices: [
+    answerChoices: [
       "a. JavaScript Operator Numerator", 
       "b. JavaScript Object Notation", 
       "c. JavaScript Object Naming"],
     answer: 1
   },
-  {
+  { num: 4,
     question: "4. Which of the following is NOT using proper Javascript syntax for commenting",
-    answersChoices: [
+    answerChoices: [
       "a. /* This is a comment. **/", 
       "b. // This is a actually a comment.", 
       "c. <!-- This is also a comment. -->"],
@@ -34,25 +44,65 @@ const questions= [
 ];
 console.log("");
 
-var score=0
-var question
 
-// Hides questions before starting quiz
+// start.style.display = "none";
+var startBtn = document.getElementById("start");
 
+function getQuestions(questionNumber) {
+  for (var i=0; i< questionBank.length; i++) {
+    var answeredQuestion = questionBank[questionNumber].question;
 
-// Pulls the questions 
-function askQuestion() {
-  var quizQuestion = 0,
-  
-
-
-  
+    var userResponse = questionBank[questionNumber].answerChoices;
+    questionText.textContent = answeredQuestion;
   }
-
 }
 
 
-// start.addEventListener("click",startQuiz);
 
-// function startQuiz() {
-//     start.style.display = "none";
+  var showQuestion = document.getElementsByName("question");
+  //   showQuestion.textContent = index.question;
+  //   console.log("please work hhaha");
+  // }
+  
+  // getQuestions(question, 1);
+  // console.log("yikes");
+  
+  
+  // function questions (){}
+  
+  
+  
+  
+  
+// //Function for Starting Quiz
+// startBtn.onclick = FUNCTION
+// const info = document.querySelector(".info");
+
+//   f
+  
+
+
+      
+  
+
+    
+
+
+ 
+
+
+
+
+
+// // Button to progress to next question
+// var nextBtn = document.getElementById("btn");
+
+// nextBtn.onlick = FUNCTION
+
+
+
+// //Button to Submit the quiz
+// var submitBtn = document.getElementById("submit");
+
+// submitBtn.onlick = FUNCTION
+
